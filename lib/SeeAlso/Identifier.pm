1c39520bb823ec0ff800238142dd30ebc4eaa3cd
Jakob Voß <jakob.voss@gbv.de>
2009-08-06 10:56:47 +0000

    Import of VOJ/SeeAlso-Server-0.57 from CPAN.
    gitpan-cpan-distribution: SeeAlso-Server
    gitpan-cpan-version:      0.57
    gitpan-cpan-path:         VOJ/SeeAlso-Server-0.57.tar.gz
    gitpan-cpan-author:       VOJ
    gitpan-cpan-maturity:     released

--- lib/SeeAlso/Identifier.pm
+++ lib/SeeAlso/Identifier.pm
@@ -5,220 +5,191 @@ use warnings;
 
 =head1 NAME
 
-SeeAlso::Identifier - an identifier passed to a SeeAlso-Server
+SeeAlso::Identifier - An identifier passed to a SeeAlso-Server
 
 =cut
 
 use Carp;
-our $VERSION = "0.43";
+our $VERSION = '0.46';
 
 =head1 DESCRIPTION
 
 The query to a SeeAlso (and other unAPI) server is just an identifier.
 By default an identifier is just a string but identifiers may also have
-special properties like checksums and normalized forms.
+special properties like checksums and canonical forms.
 
-L<SeeAlso::Identifier> models identifiers that are passed to and
+B<SeeAlso::Identifier> models identifiers that are passed to and
 processed by a L<SeeAlso::Server> and a L<SeeAlso::Source>. To
-model more complex identifiers you can either derive a subclass or
-provide additional methods to the constructor. These are a
-method to check whether the identifier is valid or not, a method
-to create a normalized form of an identifier, and a method to
-create an index entry of an identifier.
+model more complex identifiers you can either derive a subclass of
+SeeAlso::Identifier or create a new identifier class with
+L<SeeAlso::Identifier::Factory>.
 
-The concept is best explained with an example: The International
-Standard Serial Number (ISSN) is used to identify periodical
-publications. The format of the ISSN is an eight digit number,
-divided by a hyphen into two four-digit numbers. The last digit
-is a check digit which may be 0-9 or an X. In practise the hyphen
-may be missing and the check digit may also be provided lowercase.
+=cut
 
-When used as an identifier, the different forms of an ISSN as
-used in the model of L<SeeAlso::Identifier> are:
-
-=over
-
-=item value
-
-The string value as provided, for instance 'ISSN 0002-936X',
-'0002-936x', '0002936x' (which all refer to the same ISSN in
-this example)
-
-=item normalized
-
-The canonical form whith hyphem and uppercase X: '0002-936X'
-
-=item indexed 
-
-The form that is used for indexing. This could be '0002936X'
-or '0002936' because hyphen and check digit do not contain
-information. You could also store the ISSN in the 32 bit
-integer number '2996' instead of a string.
-
-=item valid
-
-The ISSN is checked by testing for invalid or missing
-characters and the check digit is computed.
-
-=back
-
-The constructor C<new> contains an example.
-
-If you provide a normalizing method C<n> then this method
-should behave like a normalizing is expected. That is for
-every possible input C<s> the condition C<n(s) == n(n(s))>
-must be true.
+use overload (
+    '""'   => sub { $_[0]->as_string },
+    'bool' => sub { $_[0]->valid },
+    '<=>' => sub { $_[0]->cmp( $_[1] ) },
+    'cmp' => sub { $_[0]->cmp( $_[1] ) },
+    fallback => 1
+);
 
 =head1 METHODS
 
-=head2 new ( [ $value | %params ] )
+=head2 new ( [ $value ] )
 
-Creates a new identifier. You can either pass either identifier's 
-value or a hash of methods with the following parameter names:
-
-=over
-
-=item valid
-
-A method to check whether the identifier is valid.
-
-=item normalized
-
-A method that returns a normalized representation of the identifier.
-
-=item indexed
-
-A method that returns an indexed representation of the identifier.
-
-=back
-
-The methods C<valid>, C<normalized>, and C<indexed> get the
-identifier's value as parameter when called.
-
-For instance the following code fragment creates an identifier
-that contains of letters only and is normalized to lowercase:
-
-  sub lcalpha {
-    my $v = shift;
-    $v =~ s/[^a-zA-Z]//g;
-    return lc($v);
-  }
-  $id = SeeAlso::Identifier->new(
-    'valid' => sub {
-      my $v = shift;
-      return $v =~ /^[a-zA-Z]+$/;
-    },
-    'normalized' => \&lcalpha
-  );
-  $id->value("AbC");
+Creates a new identifier. A value will be used to set the identifier value
+with the 'value' method with C<undef> as default value. This implies that
+the 'parse' method is called for every new identifier.
 
 =cut
 
 sub new {
-    my ($class) = shift;
+    my $class = shift;
 
-    if ($#_ <= 0) {
-        my $self = bless {
-            value => ""
-        }, $class;
-        $self->value( shift );
-        return $self;
-    }
-
-    my %params = @_;
-    my $normalized = $params{normalized};
-    my $indexed = $params{indexed};
-    my $valid = $params{valid};
-
-    croak("normalized must be a method")
-        if defined $normalized and ref($normalized) ne "CODE";
-    croak("indexed must be a method")
-        if defined $indexed and ref($indexed) ne "CODE";
-    croak("valid must be a method")
-        if defined $valid and ref($valid) ne "CODE";
-
-    my $self = bless {
-        value => undef,
-        mValid => $valid,
-        mNormalized => $normalized,
-        mIndexed => $indexed,
-    }, $class;
+    my $value = '';
+    my $self = bless \$value, $class;
+    $self->value( $_[0] );
 
     return $self;
 }
 
 =head2 value ( [ $value ] )
 
-Get and/or set the value of this identifier.
+Get (and optionally set) the value of this identifier. If you provide a value
+(including undef), it will be passed to the 'parse' function and stringified 
+afterwards to be used as the new identifier value.
 
 =cut
 
 sub value {
     my $self = shift;
-    my $value = shift;
-
-    if (defined $value) {
-        $self->{value} = $value;
+    if ( scalar @_ ) {
+        my $value = $self->parse( $_[0] );
+        $$self = defined $value ? "$value" : "";
     }
-
-    if (not defined $self->{value}) {
-        return $self->valid() ? "" : undef;
-    } else {
-        return $self->{value};
-    }
+    return $$self;
 }
 
-=head2 normalized ( )
+=head2 canonical
 
-Return a normalized representation of this identifier.
-By default this is what the value method returns.
+Returns a normalized version of the identifier. For most identifiers the
+normalized version should be an absolute URI. The default implementation
+of this method just returns the full value, so if the 'value' method already
+does normalization, you do not have to implement 'canonical'.
 
 =cut
 
-sub normalized {
-    my $self = shift;
-    if (defined $self->{mNormalized}) {
-        return &{$self->{mNormalized}}($self->{value});
-    } else {
-        return $self->value();
-    }
+sub canonical {
+    return ${$_[0]};
 }
 
-=head2 indexed ( )
+=head2 normalized
 
-Return the index value of this identifier.
-By default this is the normalized form but you may
-extend the identifier to use some kind of hash value.
+Alias for the 'canonical' method. Do not override this method but 'canonical'!
 
 =cut
 
-sub indexed {
-    my $self = shift;
-    if (defined $self->{mIndexed}) {
-        return &{$self->{mIndexed}}($self->{value});
-    } else {
-        return $self->normalized();
-    }
+sub normalized { 
+    return $_[0]->canonical;
 }
 
-=head2 valid ( )
+=head2 as_string
 
-Returns whether this identifier is valid. By default
-all non empty identifiers (everything but '' and undef)
-are valid.
+Returns an identifier object as plain string which is the canonical form.
+Identifiers are also converted to plain strings automatically by overloading.
+This means you can use identifiers as plain strings in most Perl constructs.
+
+=cut
+
+sub as_string {
+    return $_[0]->canonical;
+}
+
+=head2 hash
+
+Return a compact form of this identifier that can be used for indexing. 
+A usual compact form is the local part without namespace prefix or a 
+hash value. The default implementation of this method returns the canonical form
+of the identifier.
+
+=cut
+
+sub hash {
+    return $_[0]->canonical;
+}
+
+=head2 indexed
+
+Alias for the 'hash' method. Do not override this method but 'hash'!
+
+=cut
+
+sub indexed { 
+    return $_[0]->hash; 
+}
+
+=head2 valid
+
+Returns whether this identifier is valid - which is the case for all non-empty
+strings. This method is automatically called by overloading to derive a boolean
+value from an identifier. This means you can use identifiers as boolean values
+in most Perl constructs. Please note that in contrast to default scalars the
+identifier value '0' is valid!
 
 =cut
 
 sub valid {
+    return ${$_[0]} ne '';
+}
+
+=head2 parse ( $value )
+
+Parses a value to an identifier value of this class. This method should always
+return a string - but the return value is stringified anyway. In most cases 
+this and/or the 'canonical' method are the only methods to override in a
+subclass of SeeAlso::Identifier.
+
+This method can also be used as function. To allow the same in you subclasses' implementation, use the template:
+
+sub parse {
+    my $value = shift;
+    $value = shift if ref($value) and scalar @_;
+
+    # ... further processing of $value (validating and cleaning) ...
+
+    return defined $value ? "$value" : "";
+}
+
+=cut
+
+sub parse {
+    my $value = shift;
+    $value = shift if ref($value) and scalar @_;
+    return defined $value ? "$value" : "";
+}
+
+=head2 cmp ( $identifier )
+
+Compare two identifiers. If the supplied value is not an identifier, it
+will be converted first. By default the canonical values are compared.
+
+=cut
+
+sub cmp {
     my $self = shift;
-    if (defined $self->{mValid}) {
-        return &{$self->{mValid}}($self->{value});
-    } else {
-        return defined $self->{value} && $self->{value} ne "";
-    }
+    my $second = shift;
+    # TODO: use the same class as the first for comparing (and test this)!
+    $second = SeeAlso::Identifier->new( $second )
+        unless UNIVERSAL::isa( $second, 'SeeAlso::Identifier' );
+    return $self->canonical cmp $second->canonical;
 }
 
 1;
+
+=head2 SEE ALSO
+
+See L<URI> for the more specific Uniform Resource Identifiers.
 
 =head1 AUTHOR
 
@@ -226,7 +197,7 @@ Jakob Voss C<< <jakob.voss@gbv.de> >>
 
 =head1 LICENSE
 
-Copyright (C) 2007-2009 by Verbundzentrale Goettingen (VZG) and Jakob Voss
+Copyright (C) 2009 by Verbundzentrale Goettingen (VZG) and Jakob Voss
 
 This library is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself, either Perl version 5.8.8 or, at
